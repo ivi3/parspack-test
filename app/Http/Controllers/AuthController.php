c6c8605a3bba1db5b19190ea8b63738379a13d9b
milad <milad.parirokh@gmail.com>
2022-06-15 16:37:37 +0430

    initial project

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SignInUserRequest;
+use App\Http\Requests\SignupUserRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(SignupUserRequest $request): JsonResponse
+    {
+        $validated_fields = $request->safe()->only(['username', 'password']);
+        $user = User::create($validated_fields);
+        $token = $user->createToken("user-auth'")->plainTextToken;
+
+        $response = [
+            'access_token' => $token,
+        ];
+
+        return response()->json($response);
+    }
+
+    public function login(SignInUserRequest $request): JsonResponse
+    {
+        $validated_fields = $request->safe()->only(['username', 'password']);
+        if (!Auth::attempt($validated_fields)) {
+            return response()->json([
+                'message' => __("auth.failed")
+            ], 401);
+        }
+
+        $user = User::where('username', $request['username'])->first();
+        $token = $user->createToken('user-auth')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+        ]);
+    }
+}
