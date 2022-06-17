9dbab0a1dcb81315f5652244fd26e809c541bd7a
milad <milad.parirokh@gmail.com>
2022-06-17 18:54:05 +0430

    add UserController routes

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,10 @@ use Illuminate\Support\Facades\Route;
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->group(function (){
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/running-process', [UserController::class, 'runningProcess']);
+    Route::get('/create-directory/{name}', [UserController::class, 'createDirectory']);
+    Route::get('/create-file/{name}', [UserController::class, 'createFile']);
+    Route::get('/list-of-directories/', [UserController::class, 'listOfDirectories']);
+    Route::get('/list-of-files/', [UserController::class, 'listOfFiles']);
 });
